ea0eef25bcc380c3d10e77d62138c708bddc8319
pung-official <pungkuisin@hotmail.com>
2024-02-23 00:35:16 +0800

    fixes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require("express");
-const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 require('dotenv').config();
@@ -11,7 +10,6 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '../build')));
 app.use(cors());
 app.use(express.json());
-app.use("/", router);
 // app.listen(port, () => console.log("Server Running"));
 // console.log(process.env.EMAIL_USER);
 // console.log(process.env.EMAIL_PASS);
@@ -32,7 +30,7 @@ contactEmail.verify((error) => {
   }
 });
 
-router.post("/contact", (req, res) => {
+app.post("/contact", (req, res) => {
   const name = req.body.firstName + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
